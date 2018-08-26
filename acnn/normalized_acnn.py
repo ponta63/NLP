d2f748602415ab2d0eba638d5868bee84eadf994
ponta63 <m63kondo@gmail.com>
2018-08-27 00:23:43 +0900

    modified trivial points.

--- acnn/normalized_acnn.py
+++ acnn/normalized_acnn.py
@@ -8,6 +8,8 @@ from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.framework import tensor_shape
 
 
+##  It has not been implemented yet. (It has not been published yet.)
+
 ####################################################
 ####################################################
 ####################################################
