8e89c0b782c5ace72f1f2e83b530f52ef074aa3d
ponta63 <m63kondo@gmail.com>
2018-08-26 23:47:02 +0900

    push dummy_code

---
+++ acnn/acnn.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+# -*- encoding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.framework import tensor_shape
+
+
+####################################################
+####################################################
+####################################################
+class ACNN(object):
+	pass
+
+
+####################################################
+####################################################
+####################################################
+class ACNN3Shared(ACNN):
+	pass
---
+++ acnn/normalized_acnn.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+# -*- encoding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.framework import tensor_shape
+
+
+####################################################
+####################################################
+####################################################
+class NormalizedACNN(object):
+	pass
+
+
+####################################################
+####################################################
+####################################################
+class NormalizedACNN3Shared(NormalizedACNN):
+	pass
+
